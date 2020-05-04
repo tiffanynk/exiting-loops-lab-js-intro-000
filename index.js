8371e44d72ce4d5a367a030f49a945e730ac4bcd
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-05-04 17:52:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function breakOut(array, changeValue, stopValue) {
-  
+  for (let i = 0; 1 = array.length; i < 1; i++) {
+    if (array[i] === stopValue) {
+      break
+    }
+    array[i] = changeValue;
+  }
+  return array
 }
 
 function keepGoing(array, changeValue, skipValue) {
